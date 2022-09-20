ace8b7de8aa62b2a314f085b546f85596a143e53
Tomislav Tipurić <totipu@nephos.eu>
2022-09-20 23:08:24 +0200

    Added swagger documentation with swagger-autogen

--- user-api/src/index.js
+++ user-api/src/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const UsersRouter = require('./users/users.router');
 var morgan = require('morgan');
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('../swagger_output.json')
 
 const app = express();
 
@@ -9,6 +11,7 @@ app.use(express.json());
 // log every HTTP request in Apache combined format
 app.use(morgan('combined'));
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use('/', UsersRouter)
 
 const port = process.env.PORT || "3000";
--- user-api/src/users/users.router.js
+++ user-api/src/users/users.router.js
@@ -22,12 +22,62 @@ const userController = UserController();
 
 const router = Router();
 
-router.get('/users/healthcheck', (req, res) => { return res.status(200).json({ "Status": "Healthy"})});
-router.post('/users', userController.createUser);
-router.get('/users/:id', userController.getUserById);
-router.get('/users', userController.getAllUsers);
-router.put('/users/:id', userController.updateUser);
-router.delete('/users/:id', userController.deleteUser);
+router.get('/users/healthcheck', (req, res) => { 
+   /* #swagger.tags = ['Users'] */
+   return res.status(200).json({ "Status": "Healthy"})});
+router.post('/users', userController.createUser
+
+   /* #swagger.tags = ['Users']
+      #swagger.parameters['obj'] = {
+      in: 'body',
+      description: 'User object',
+      required: true,
+      type: 'object',
+      schema: { $ref: "#/definitions/User" }
+   } */
+
+);
+router.get('/users/:id', userController.getUserById
+
+   /*	#swagger.tags = ['Users']
+      #swagger.parameters['id'] = {
+      in: 'path',
+      description: 'User ID',
+      required: true
+   } */
+
+);
+router.get('/users', userController.getAllUsers
+   /* #swagger.tags = ['Users'] */
+);
+router.put('/users/:id', userController.updateUser
+
+   /*	#swagger.tags = ['Users']
+      #swagger.parameters['id'] = {
+      in: 'path',
+      description: 'User ID',
+      required: true
+   } */
+
+   /* #swagger.parameters['obj'] = {
+      in: 'body',
+      description: 'User object',
+      required: true,
+      type: 'object',
+      schema: { $ref: "#/definitions/User" }
+   } */
+
+);
+router.delete('/users/:id', userController.deleteUser
+
+   /*	#swagger.tags = ['Users']
+      #swagger.parameters['id'] = {
+      in: 'path',
+      description: 'User ID',
+      required: true
+   } */
+
+);
 
 
 module.exports = router;
